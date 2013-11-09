9d0113a7f12b1858afcc1ba6820b413ac65ee2d7
John-Peter Berntsson <joppe@John-Peters-MacBook-Pro.local>
2013-11-09 22:49:13 +0100

    Establishing connection to mongodb

--- src/main/java/com/sevendown/App.java
+++ src/main/java/com/sevendown/App.java
@@ -1,13 +1,21 @@
 package com.sevendown;
 
-/**
- * Hello world!
- *
- */
+import java.net.UnknownHostException;
+import java.util.Set;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws UnknownHostException
     {
-        System.out.println( "Hello World!" );
+        MongoClient mongoClient = new MongoClient( "localhost" );
+        DB db = mongoClient.getDB( "toodo" );
+        Set<String> colls = db.getCollectionNames();
+
+        for (String s : colls) {
+            System.out.println(s);
+        }
     }
 }
