0c616a117cc1e4ca64e37007511394dd1851c341
John-Peter Berntsson <joppe@John-Peters-MacBook-Pro.local>
2013-11-17 12:23:28 +0100

    Added basic functionality to add list

--- src/main/java/com/sevendown/App.java
+++ src/main/java/com/sevendown/App.java
@@ -1,21 +1,28 @@
 package com.sevendown;
 
 import java.net.UnknownHostException;
-import java.util.Set;
 
 import com.mongodb.DB;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-
+import static spark.Spark.*;
+import spark.*;
+import com.mongodb.util.JSON;
 public class App 
 {
     public static void main( String[] args ) throws UnknownHostException
     {
         MongoClient mongoClient = new MongoClient( "localhost" );
-        DB db = mongoClient.getDB( "toodo" );
-        Set<String> colls = db.getCollectionNames();
+        final DB db = mongoClient.getDB( "toodo" );
 
-        for (String s : colls) {
-            System.out.println(s);
-        }
+        post(new Route("/lists") {
+            @Override
+            public Object handle(Request request, Response response) {
+            	//TO-DO validate input
+            	return db.getCollection("lists").insert((DBObject)JSON.parse(request.body()));
+            }
+         });
+
+
     }
 }
